549c8c977d6ee2a67afb64326af52481ae4ea58d
Myskeez <my.skeez1@gmail.com>
2026-03-03 16:37:17 +0100

    Make two versions for glitchy text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import '../styles/home.css'
+import GlitchText_V1 from '../components/GlitchText_V1.tsx'
+import GlitchText_V2 from '../components/GlitchText_V2.tsx'
 
 export default function Home() {
     return (
         <section>
-            <h1 className='slogan'>Simplicity in design.<br />Harmony in function.</h1>
+            {/*<h1 className='slogan s-top'>Simplicity in design.</h1>
+            <h1 className='slogan s-btm'>Harmony in function.</h1>*/}
+            {/*<GlitchText_V1 className='slogan s-top' as='h1' text='Simplicity in design.' />
+            <GlitchText_V1 className='slogan s-btm' as='h1' text='Harmony in function.' />*/}
+            <GlitchText_V2 className='slogan s-top' as='h1' text='Simplicity in design.' />
+            <GlitchText_V2 className='slogan s-btm' as='h1' text='Harmony in function.' />
             <p>This is my portfolio homepage.</p>
             <p>And also my very first project with React, Vite and React Router - YAY!</p>
             <br/>
