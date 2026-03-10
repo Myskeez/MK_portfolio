19da5297104741afdaf40461ca3712c37c0bdc91
Myskeez <my.skeez1@gmail.com>
2026-03-10 16:28:39 +0100

    Adjust styling of general h1 headings

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <section>
-            <h1>About</h1>
+            <h1>Ab<span style={{color: 'var(--accent-color_2)'}}>out</span></h1>
             <p>Short bio, skills, tech stack, what I'm looking for.</p>
         </section>
     )
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <section>
-            <h1>Contact</h1>
+            <h1>C<span style={{color: 'var(--accent-color_2)'}}>on</span>tact</h1>
             <p>Email:
                 <a href="mailto:max.schwarz2003@gmail.com"> max.schwarz2003@gmail.com</a>
             </p>
